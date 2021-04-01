969a83c3ae649031d6bec93fb49636af990c6e7a
jng2 <jng2@luc.edu>
2021-04-01 11:52:21 -0500

    [CODE] Docker Image Update
    Changed the docker image to point to the new lab one.  The image is the same as it was before

--- NA12878_comparison/bcftovcf.sh
+++ NA12878_comparison/bcftovcf.sh
@@ -10,7 +10,7 @@ NAME=${f##*/}
 NAME=${NAME%%.*}
 echo $NAME
 
-bsub -q general -g /jeffrey.ng/200test -R 'affinity[core(1)] span[ptile=4] rusage[mem=16GB]'  -G compute-tychele   -a 'docker(jng2/testrepo2_actual:bcftools)' "/opt/conda/bin/bcftools view $f | /opt/conda/bin/bgzip -c > /storage1/fs1/tychele/Active/testing/ris_testing/parabricks/NA12878_extras/dv_vcf/$NAME.trio_test_NA12878.glnexus.dv.vcf.gz"
+bsub -q general -g /jeffrey.ng/200test -R 'affinity[core(1)] span[ptile=4] rusage[mem=16GB]'  -G compute-tychele   -a 'docker(tnturnerlab/gpu_accelerated_denovo_workflow:glnexus_filter)' "/opt/conda/bin/bcftools view $f | /opt/conda/bin/bgzip -c > /storage1/fs1/tychele/Active/testing/ris_testing/parabricks/NA12878_extras/dv_vcf/$NAME.trio_test_NA12878.glnexus.dv.vcf.gz"
 
 done
 
@@ -21,6 +21,6 @@ NAME=${NAME%%.*}
 echo $NAME
 
 
-bsub -q general -g /jeffrey.ng/200test -R 'affinity[core(1)] span[ptile=4] rusage[mem=16GB]'  -G compute-tychele   -a 'docker(jng2/testrepo2_actual:bcftools)' "/opt/conda/bin/bcftools view $f | /opt/conda/bin/bgzip -c > /storage1/fs1/tychele/Active/testing/ris_testing/parabricks/NA12878_extras/hc_vcf/$NAME.trio_test_NA12878.glnexus.hc.vcf.gz"
+bsub -q general -g /jeffrey.ng/200test -R 'affinity[core(1)] span[ptile=4] rusage[mem=16GB]'  -G compute-tychele   -a 'docker(tnturnerlab/gpu_accelerated_denovo_workflow:glnexus_filter)' "/opt/conda/bin/bcftools view $f | /opt/conda/bin/bgzip -c > /storage1/fs1/tychele/Active/testing/ris_testing/parabricks/NA12878_extras/hc_vcf/$NAME.trio_test_NA12878.glnexus.hc.vcf.gz"
 
 done
